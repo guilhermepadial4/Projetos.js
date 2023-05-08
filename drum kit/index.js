ba00d3eef22e26e3b1017dc0781e5dcd49324d49
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-05-08 11:36:13 -0300

    Update projeto drum kit

--- drum kit/index.js
+++ drum kit/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const souds={
+const container=document.getElementById("container")
+
+const sounds={
     "A":'boom.wav',
     "S":'clap.wav',
     "D":'hihat.wav',
@@ -14,14 +16,48 @@ const souds={
 
 const create_div=(text)=>{
     const div=document.createElement("div");
-    div.setAttribute("class","key");
+    div.classList.add("key");
     div.textContent=text;
     div.id=text;
-    document.getElementById("container").appendChild(div);
+    container.appendChild(div);
 }
 
-const display=(souds)={
-    
+const display=(sounds)=>{
+    Object.keys(sounds).forEach(create_div);
 }
 
-display(souds);
+const playSound=(letter)=>{
+    const audio=new Audio(`./sounds/${sounds[letter]}`);
+    audio.play();
+}
+
+const addEffect=(letter)=>{ 
+    document.getElementById(letter).classList.add("active");
+}
+
+const removeEffect=(letter)=>{
+    const div=document.getElementById(letter);
+    const removeActive=()=> div.classList.remove("active");
+    div.addEventListener("transitionend",removeActive);
+}
+
+const activate_div=(evt)=>{
+    let letter=""
+    if(evt.type == "click"){
+         letter=evt.target.id;
+    }else{
+        letter=evt.key.toUpperCase();
+    }
+    const letterallowed=sounds.hasOwnProperty(letter);
+    if(letterallowed){
+        addEffect(letter);
+        playSound(letter);
+        removeEffect(letter);
+    }
+}
+
+display(sounds);
+
+container.addEventListener("click",activate_div);
+
+window.addEventListener("keydown",activate_div)
