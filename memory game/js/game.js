bb788f0476eaad505997cdac0035039fa43680e0
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-06-05 11:05:25 -0300

    Update the project

---
+++ memory game/js/game.js
@@ -0,0 +1,40 @@
+const grid = document.querySelector(".grid")
+
+const characters = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'summer',
+    'meeseeks',
+    'scroopy'
+]
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+const createCard = (character) => {
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+    
+    card.appendChild(front);
+    card.appendChild(back);
+
+    return card;
+}
+
+const loadGame = () => {
+    characters.forEach((characters) => {
+        const card = createCard();
+        grid.appendChild(card);
+    });
+}
+
+loadGame();
