8d014882f1affc060b75d01b0b86213d0e8f0abd
guilhermepadial4 <guilherme.padial4@gmail.com>
2023-06-07 11:06:36 -0300

    Update the project Memory Game

--- memory game/js/game.js
+++ memory game/js/game.js
@@ -14,7 +14,40 @@ const characters = [
 ]
 
 let firstCard = '';
-let secondCard = ''
+let secondCard = '';
+
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll("disabled-card");
+
+    if(disabledCards.length === 20){
+        alert("Parabéns ,você conseguiu")
+    }
+}
+
+const checkCards = () => {
+    const firstCharacter = firstCard.getAttribute('data-character');
+    const secondCharacter = secondCard.getAttribute('data-character');
+
+    if(firstCharacter === secondCharacter){
+        firstCard.firstChild.classList.add('disabled-card');
+        secondCard.firstChild.classList.add('disabled-card');
+
+        firstCard = '';
+        secondCard = '';
+
+        checkEndGame();
+    }else{
+
+        setTimeout(() => {
+            firstCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+
+            firstCard = '';
+            secondCard = '';
+        }, 500);
+    }
+}
+
 const createElement = (tag, className) => {
     const element = document.createElement(tag);
     element.className = className;
@@ -28,9 +61,15 @@ const revealCard = ({ target }) => {
 
     if(firstCard === ''){
         target.parentNode.classList.add('reveal-card');
+        firstCard = target.parentNode;
+
+    }else if (secondCard === ''){
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode;
+
+        checkCards();
     }
 
-    target.parentNode.classList.add('reveal-card');
 }
 
 const createCard = (character) => {
@@ -38,12 +77,13 @@ const createCard = (character) => {
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
     
-    front.style.backgroundImage = `url('../images/${character}.png')`
+    front.style.backgroundImage = `url('../images/${character}.png')`;
 
     card.appendChild(front);
     card.appendChild(back);
 
-    card.addEventListener('click',revealCard)
+    card.addEventListener('click',revealCard);
+    card.setAttribute('data-character', character);
 
     return card;
 }
