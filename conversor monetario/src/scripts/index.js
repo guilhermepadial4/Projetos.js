5d5383348f7369808c6277b02d1b7aad30fa040d
Guilherme Padial <guilherme.padial4@gmail.com>
2023-12-27 11:29:59 -0300

    Update the index.js

--- conversor monetario/src/scripts/index.js
+++ conversor monetario/src/scripts/index.js
@@ -1,7 +1,13 @@
 import { renderDarkMode } from "./renderDarkMode.js";
 
 const checkInput = () => {
-    
+    const buttonConvert = document.querySelector('.convert__button');
+
+    buttonConvert.addEventListener('click', () => {
+        console.log('test');
+    });
 }
+
+checkInput();
 
 renderDarkMode('src/assets/svg/dark_mode.svg', 'src/assets/svg/light_mode.svg');
