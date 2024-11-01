b02cfa43e4c7e48aa40554f7ef18423afa660e5e
Guilherme Padial <guilherme.padial4@gmail.com>
2024-11-01 10:30:52 -0300

    feat: initial commit

---
+++ portfolio/my-portfolio/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Image src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
+    </main>
+  );
+}
