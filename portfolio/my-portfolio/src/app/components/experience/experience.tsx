074c57289e166cc31721071d2a94ebe6f7769566
Guilherme Padial <guilherme.padial4@gmail.com>
2024-11-06 11:54:53 -0300

    Update the project

--- portfolio/my-portfolio/src/app/components/experience/experience.tsx
+++ portfolio/my-portfolio/src/app/components/experience/experience.tsx
@@ -1,4 +1,6 @@
 import { SectionTitle } from "../sectionTitle/section-title";
+import Image from "next/image";
+
 import "./experience.scss";
 
 export function Experience() {
@@ -7,7 +9,24 @@ export function Experience() {
       <SectionTitle text="experience" />
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid iure quidem eos magni repellat voluptatum</p>
 
-      <div className="experience-time"></div>
+      <div className="experience-time">
+        <div className="experience__languages">
+          <Image src="/react.png" alt="React logo" width={40} height={40} priority />
+          <div className="experience__unit">
+            <div className="experience__measure"></div>
+          </div>
+
+          <Image src="/js.png" alt="Javascriot logo" width={40} height={40} priority />
+          <div className="experience__unit">
+            <div className="experience__measure"></div>
+          </div>
+
+          <Image src="/ts.png" alt="Typescript logo" width={40} height={40} priority />
+          <div className="experience__unit">
+            <div className="experience__measure"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
