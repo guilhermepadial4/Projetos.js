44f308bca5bee94151871121027184d0ee1e8d2a
Guilherme Padial <guilherme.padial4@gmail.com>
2024-11-07 10:16:40 -0300

    Update the project

--- portfolio/my-portfolio/src/app/components/experience/experience.tsx
+++ portfolio/my-portfolio/src/app/components/experience/experience.tsx
@@ -6,24 +6,34 @@ import "./experience.scss";
 export function Experience() {
   return (
     <div className="experience">
-      <SectionTitle text="experience" />
+      <SectionTitle text="Experience" />
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid iure quidem eos magni repellat voluptatum</p>
 
-      <div className="experience-time">
+      <div className="experience__time">
         <div className="experience__languages">
           <Image src="/react.png" alt="React logo" width={40} height={40} priority />
           <div className="experience__unit">
-            <div className="experience__measure"></div>
+            <div className="experience__measure measure-2">
+              <span>2 years</span>
+            </div>
           </div>
+        </div>
 
+        <div className="experience__languages">
           <Image src="/js.png" alt="Javascriot logo" width={40} height={40} priority />
           <div className="experience__unit">
-            <div className="experience__measure"></div>
+            <div className="experience__measure measure-3">
+            <span>3 years</span>
+            </div>
           </div>
+        </div>
 
-          <Image src="/ts.png" alt="Typescript logo" width={40} height={40} priority />
+        <div className="experience__languages">
+          <Image src="/ts.png" alt="Javascriot logo" width={40} height={40} priority />
           <div className="experience__unit">
-            <div className="experience__measure"></div>
+            <div className="experience__measure measure-1">
+            <span>1 year</span>
+            </div>
           </div>
         </div>
       </div>
